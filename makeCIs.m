7b1c4fda400fd3576dea90317fe6c659311715db
Olav Krigolson <krigolson@gmail.com>
2020-04-08 12:38:10 -0700

    Yup

--- makeCIs.m
+++ makeCIs.m
@@ -1,17 +1,27 @@
-function [CIs] = makeCIs(data)
+function CI = makeCIs(data)
 
-    % this function computes CIs for a vector of data
+    % this function computes CIs for a matrix of data
     % O. Krigolson June 2018
 
     n = size(data,1);
     
     for counter = 1:size(data,2)
     
+        tempData = [];
+        tempData = data(:,counter);
+        
         df = n - 1;
-        t_critical = tinv(0.025,df);
-        std_data = std(data(:,counter));
+        % use 0.05 as it is half of (1-0.95)/2
+        tCritical = tinv(0.025,df);
+        sdData = std(tempData);
         sqrtn = sqrt(n);
-        CIs(counter) = abs(t_critical*std_data/sqrtn);
+        
+        ci = abs(tCritical*sdData/sqrtn);
+        
+        CI(1,counter) = mean(tempData);
+        CI(2,counter) = ci;
+        CI(3,counter) = mean(tempData) - ci;
+        CI(4,counter) = mean(tempData) + ci;
         
     end
 
